07e46a6014a05c6a7a8222bb3e9583a3626f3725
Charlie Friend <charles.d.friend@gmail.com>
2018-04-26 21:56:19 -0700

    Add template and view for create game form.

--- play/urls.py
+++ play/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from apps.play.views.home import home
+from apps.play.views.create import create
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^create/$', create, name='create'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
