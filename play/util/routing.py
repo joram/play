e616cdb5b0db7cc05dcefda6c39dc12c52cc5366
colin <colin@coldog.mint>
2018-06-24 19:44:04 -0700

    Break out an authentication app.

---
+++ play/util/routing.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseNotAllowed
+
+
+def method_dispatch(**table):
+    def invalid_method(request, *args, **kwargs):
+        return HttpResponseNotAllowed(table.keys())
+
+    def d(request, *args, **kwargs):
+        method = request.method
+        if request.POST and '_method' in request.POST:
+            method = request.POST['_method']
+        method = method.upper()
+        handler = table.get(method, invalid_method)
+        return handler(request, *args, **kwargs)
+    return d
