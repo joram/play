7c4e4e2e62222b1707ae4c556d39ed3f19bbba5c
Brandon Brown <brandon@brandonb.ca>
2018-07-18 15:18:11 -0700

    Cleans up the Game creation page
    Also upgrades to latest Bootstrap minor version

--- play/apps/game/forms.py
+++ play/apps/game/forms.py
@@ -3,23 +3,35 @@ from apps.game.engine import run_game
 
 
 class GameForm(forms.Form):
+    DEFAULT_HEIGHT = 20
+    DEFAULT_WIDTH = 20
+    DEFAULT_FOOD = 5
     MAX_SNAKES = 10
 
-    width = forms.IntegerField()
-    height = forms.IntegerField()
-    food = forms.IntegerField()
+    width = forms.IntegerField(initial=DEFAULT_WIDTH)
+    height = forms.IntegerField(initial=DEFAULT_HEIGHT)
+    food = forms.IntegerField(initial=DEFAULT_FOOD)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for i in range(self.MAX_SNAKES):
+        for i in range(1, self.MAX_SNAKES + 1):
             self.fields[f'snake_name_{i}'] = forms.CharField(
-                max_length=100, label=f'Snake {i} Name',
+                max_length=100,
+                label=f'Snake {i} Name',
                 required=False,
+                widget=forms.TextInput()
             )
+
             self.fields[f'snake_url_{i}'] = forms.CharField(
-                max_length=100, label=f'Snake {i} Url',
+                max_length=100,
+                label=f'Snake {i} Url',
                 required=False,
+                widget=forms.URLInput()
             )
+
+            # Note: This "groups" is used to create the form on the front end
+            self.fields[f'snake_name_{i}'].group = 'snake'
+            self.fields[f'snake_url_{i}'].group = 'snake'
 
     def clean(self):
         cleaned_data = super().clean()
