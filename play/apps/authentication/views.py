59acf33b0d931afc17970e0b2a11996daa1b7eab
Brandon Brown <brandon@brandonb.ca>
2018-08-01 22:59:36 -0700

    Adds a proper logout mechanism

--- play/apps/authentication/views.py
+++ play/apps/authentication/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import redirect
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 
 @login_required
 def index(request):
     return redirect('/team')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
+
+
--- play/settings/base.py
+++ play/settings/base.py
@@ -62,7 +62,7 @@ if is_production_env():
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
