5c33b5fb9f3022e911b1239b6825711c6ecaba9e
Brandon Brown <brandon@brandonb.ca>
2018-08-20 22:10:31 -0700

    Fix the broken tests

--- play/apps/game/views/games_test.py
+++ play/apps/game/views/games_test.py
@@ -24,10 +24,18 @@ def test_create(mock_run, client):
     id = 'a879f127-55c2-4b0c-99c9-bce09c9fc0cf'
     mock_run.return_value = id
     user_factory.login_as(client)
-    response = client.post(f'/games/', {
+    response = client.post(f'/games/new/', {
         'width': 10,
         'height': 10,
         'food': 10,
+        # snake formset
+        'snake-TOTAL_FORMS': 1,
+        'snake-INITIAL_FORMS': 0,
+        'snake-MIN_NUM_FORMS': 1,
+        'snake-MAX_NUM_FORMS': 10,
+        'snake-0-name': 'foo',
+        'snake-0-url': 'http://example.com',
     })
+
     assert response.status_code == 302
     assert len(mock_run.call_args_list) == 1
