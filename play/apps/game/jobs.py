b9cf955dfcd4e8cdcb66226c12f6d6340f5c34a3
Colin Walker <colinwalker270@gmail.com>
2018-09-19 17:14:10 -0700

    Merge in from master.

--- play/apps/game/jobs.py
+++ play/apps/game/jobs.py
@@ -2,11 +2,18 @@ from apps.game.models import Game
 
 
 class GameStatusJob:
+<<<<<<< Updated upstream
     """ A job that iterates over all active games and refreshes them. """
     active_statuses = (
         Game.Status.PENDING,
         Game.Status.RUNNING,
         Game.Status.STOPPED,
+=======
+    """ A job that iterates over all active gamees and refreshes them. """
+
+    filters = dict(
+        status__in=(Game.Status.PENDING, Game.Status.RUNNING, Game.Status.STOPPED)
+>>>>>>> Stashed changes
     )
 
     def run(self):
