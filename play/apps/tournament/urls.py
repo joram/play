f598dbadb2312bf9745b240f22d5a5a6a7a825ba
John <john@sendwithus.com>
2019-01-16 14:58:25 -0800

    rework the view for tournament brackets to have a drop down (it was getting cluttered)

--- play/apps/tournament/urls.py
+++ play/apps/tournament/urls.py
@@ -41,6 +41,9 @@ urlpatterns = [
     url(r'^tournament/bracket/(?P<id>\w+)/$', method_dispatch(
         GET=tournament_views.tournament_bracket.show,
     )),
+    url(r'^tournament/bracket/(?P<id>\w+)/csv$', method_dispatch(
+        GET=tournament_views.tournament_bracket.show_csv,
+    )),
     url(r'^tournament/bracket/(?P<id>\w+)/create/next/round$', method_dispatch(
         GET=tournament_views.tournament_bracket.create_next_round,
     )),
