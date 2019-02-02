42495d9763bd06ff98d91adfafb88dfd521b5845
Daniel Steuernol <dlsteuer@gmail.com>
2019-02-01 21:21:49 -0800

    add feed of games to homepage

--- play/apps/authentication/views.py
+++ play/apps/authentication/views.py
@@ -2,9 +2,33 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+from apps.game.models import Game
+from apps.utils.helpers import generate_game_url
+
 
 def index(request):
-    return render(request, "home.html")
+
+    statuses = (
+        Game.Status.PENDING,
+        Game.Status.RUNNING,
+        Game.Status.STOPPED,
+        Game.Status.COMPLETE,
+    )
+    games = Game.objects.filter(status__in=statuses).order_by("-created")[:6]
+    return render(
+        request,
+        "home.html",
+        {
+            "games": [
+                {
+                    "url": generate_game_url(g.engine_id)
+                    + "&autoplay=true&hideScoreboard=true",
+                    "engine_id": g.engine_id,
+                }
+                for g in games
+            ]
+        },
+    )
 
 
 @login_required
