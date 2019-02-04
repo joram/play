8d979c242a803d82ecf3f6d9e826ddd64a9d1a35
Colin Walker <colin@sendwithus.com>
2019-02-03 17:17:59 -0800

    Add tests for public page.

---
+++ play/apps/platform/views/players_test.py
@@ -0,0 +1,12 @@
+from apps.platform.factories import PlayerFactory
+from apps.authentication.factories import UserFactory
+
+user_factory = UserFactory()
+player_factory = PlayerFactory()
+
+
+def test_get(client):
+    user = user_factory.login_as(client)
+    player = player_factory.player(user)
+    response = client.get(f"/players/{player.username}")
+    assert response.status_code == 200
