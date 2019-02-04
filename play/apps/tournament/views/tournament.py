d05411d6ce063defdc82de5987c05a1564437797
Daniel Steuernol <dlsteuer@gmail.com>
2019-02-04 14:06:17 -0800

    some fixes for view current tournament game

--- play/apps/tournament/views/tournament.py
+++ play/apps/tournament/views/tournament.py
@@ -74,9 +74,9 @@ def show_current_game(request, tournament_id):
     if request.GET.get("json") == "true":
         return JsonResponse({"tournament": {"casting_uri": tournament.casting_uri}})
 
-    return render(request, "tournament/show_current_game.html", {
-        "tournament": tournament,
-    })
+    return render(
+        request, "tournament/show_current_game.html", {"tournament": tournament}
+    )
 
 
 @admin_required
@@ -84,16 +84,15 @@ def show_current_game(request, tournament_id):
 @transaction.atomic
 def cast_current_game(request, tournament_id):
     tournament = Tournament.objects.get(id=tournament_id)
-    heat_game = HeatGame.objects.filter(id=request.POST.get("heat_game_id"))
-    if not heat_game.exists():
-        # problem
-        pass
+    heat_game = HeatGame.objects.get(id=request.POST.get("heat_game_id"))
 
     if heat_game.game is None or heat_game.game.engine_id is None:
         heat_game.game.create()
         heat_game.game.run()
 
-    tournament.casting_uri = generate_game_url(heat_game.first().game.engine_id)+"&autoplay=true&countdown=10"
+    tournament.casting_uri = (
+        generate_game_url(heat_game.game.engine_id) + "&autoplay=true&countdown=10"
+    )
     tournament.save()
 
     rounds = Round.objects.filter(tournament_bracket__in=tournament.brackets)
