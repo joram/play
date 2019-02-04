1653d81acf01e6824d6da19ef0c29aaf6d247a1b
Colin Walker <colin@sendwithus.com>
2019-02-03 18:06:02 -0800

    Add snake model.

--- play/apps/platform/factories.py
+++ play/apps/platform/factories.py
@@ -1,6 +1,16 @@
-from apps.platform.models import Player
+from apps.platform.models import Player, Snake
 
 
 class PlayerFactory:
     def player(self, user):
         return Player.objects.create(user=user)
+
+
+class SnakeFactory:
+    def snake(self, player, n=1, commit=True):
+        if n > 1:
+            return [self.basic(player, commit=commit) for _ in range(n)]
+        snake = Snake(name="test", url="http://foo.bar", player=player)
+        if commit:
+            snake.save()
+        return snake
