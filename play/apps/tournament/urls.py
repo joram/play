08c3abe0d65115bef61dcb10175a97c2334864fc
Brandon Brown <brandon@brandonb.ca>
2019-02-07 12:42:07 -0800

    Remove team from navbar (#100)

--- play/apps/tournament/urls.py
+++ play/apps/tournament/urls.py
@@ -1,124 +1,119 @@
-from django.conf.urls import url
+from django.urls import re_path
 from apps.tournament import views as tournament_views
-from util.routing import method_dispatch
+from util.routing import method_dispatch as route
 
 urlpatterns = [
-    url(
+    re_path(
         r"^team/$",
-        method_dispatch(
-            GET=tournament_views.team.index, PUT=tournament_views.team.update
-        ),
+        route(GET=tournament_views.team.index, PUT=tournament_views.team.update),
     ),
-    url(
+    re_path(
         r"^team/new/$",
-        method_dispatch(GET=tournament_views.team.new, POST=tournament_views.team.new),
+        route(GET=tournament_views.team.new, POST=tournament_views.team.new),
     ),
-    url(r"^team/edit/$", method_dispatch(GET=tournament_views.team.edit)),
-    url(r"^team/members/$", method_dispatch(GET=tournament_views.members.index)),
-    url(
+    re_path(r"^team/edit/$", route(GET=tournament_views.team.edit)),
+    re_path(r"^team/members/$", route(GET=tournament_views.members.index)),
+    re_path(
         r"^team/members/new/$",
-        method_dispatch(
-            GET=tournament_views.members.new, POST=tournament_views.members.new
-        ),
+        route(GET=tournament_views.members.new, POST=tournament_views.members.new),
     ),
-    url(
-        r"^team/members/(?P<id>\w+)/$",
-        method_dispatch(DELETE=tournament_views.members.delete),
+    re_path(
+        r"^team/members/(?P<id>\w+)/$", route(DELETE=tournament_views.members.delete)
     ),
-    url(
+    re_path(
         r"^tournament/(?P<tournament_id>\w+)/bracket/new/$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_bracket.new,
             POST=tournament_views.tournament_bracket.new,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<bracket_id>\w+)/edit/$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_bracket.edit,
             POST=tournament_views.tournament_bracket.edit,
             PUT=tournament_views.tournament_bracket.edit,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/(?P<tournament_id>\w+)/compete$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_snake.compete,
             POST=tournament_views.tournament_snake.compete,
             PUT=tournament_views.tournament_snake.compete,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/snake/(?P<tournament_snake_id>\w+)/edit",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_snake.edit,
             POST=tournament_views.tournament_snake.edit,
             PUT=tournament_views.tournament_snake.edit,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/snake/(?P<tournament_snake_id>\w+)/delete",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_snake.delete,
             POST=tournament_views.tournament_snake.delete,
             PUT=tournament_views.tournament_snake.delete,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/$",
-        method_dispatch(GET=tournament_views.tournament_bracket.show),
+        route(GET=tournament_views.tournament_bracket.show),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/csv$",
-        method_dispatch(GET=tournament_views.tournament_bracket.show_csv),
+        route(GET=tournament_views.tournament_bracket.show_csv),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/tree$",
-        method_dispatch(GET=tournament_views.tournament_bracket.tree),
+        route(GET=tournament_views.tournament_bracket.tree),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/set-casting",
-        method_dispatch(GET=tournament_views.tournament_bracket.cast_page),
+        route(GET=tournament_views.tournament_bracket.cast_page),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/create/next/round$",
-        method_dispatch(GET=tournament_views.tournament_bracket.create_next_round),
+        route(GET=tournament_views.tournament_bracket.create_next_round),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<bracket_id>\w+)/update/games",
-        method_dispatch(GET=tournament_views.tournament_bracket.update_game_statuses),
+        route(GET=tournament_views.tournament_bracket.update_game_statuses),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/heat/(?P<heat_id>\w+)/create_game/$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament_bracket.create_game,
             POST=tournament_views.tournament_bracket.create_game,
         ),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/heat/(?P<heat_id>\w+)/game/(?P<heat_game_number>\w+)/$",
-        method_dispatch(GET=tournament_views.tournament_bracket.run_game),
+        route(GET=tournament_views.tournament_bracket.run_game),
     ),
-    url(
+    re_path(
         r"^tournament/bracket/(?P<id>\w+)/heat/(?P<heat_id>\w+)/game/(?P<heat_game_number>\w+)/delete",
-        method_dispatch(GET=tournament_views.tournament_bracket.delete_game),
+        route(GET=tournament_views.tournament_bracket.delete_game),
     ),
-    url(r"^tournaments/$", method_dispatch(GET=tournament_views.tournament.index)),
-    url(
+    re_path(r"^tournaments/$", route(GET=tournament_views.tournament.index)),
+    re_path(
         r"^tournament/new/$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament.new, POST=tournament_views.tournament.new
         ),
     ),
-    url(
+    re_path(
         r"^tournament/(?P<id>\w+)/edit/$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament.edit, POST=tournament_views.tournament.edit
         ),
     ),
-    url(
+    re_path(
         r"^tournament/(?P<tournament_id>\w+)/current_game$",
-        method_dispatch(
+        route(
             GET=tournament_views.tournament.show_current_game,
             POST=tournament_views.tournament.cast_current_game,
         ),
